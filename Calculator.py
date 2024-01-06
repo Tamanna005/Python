db7730ba38cabc964fc2ec2e236bff8407e7684f
Tamanna Garva <155551840+Tamanna005@users.noreply.github.com>
2024-01-07 01:25:24 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+x = input('Enter first number:')
+y = input('Enter a operator:"/,+,-,*,% "')
+z = input('Enter second number:')
+
+x = int(x)
+z = int(z)
+
+if('+'):
+    print(x+z)
+elif('-'):
+    print(x-z)
+elif('*'):
+    print(x*z)
+elif('/'):
+    print(x/z)
+elif('%'):
+    print(x%z)
+    
+
